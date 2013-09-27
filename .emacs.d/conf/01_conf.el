72534b6d25a4a50a07daaa86a12e96848269367d
Kosuke Asami <tfortress58@gmail.com>
2013-09-28 02:30:11 +0900

    emacs: delete the hiline color settings

--- .emacs.d/conf/01_conf.el
+++ .emacs.d/conf/01_conf.el
@@ -79,17 +79,6 @@
 (setq mouse-wheel-follow-mouse t)
 
 ;; hiline
-(defface hlline-face
-  '((((class color)
-      (background dark))
-     (:background "dark slate gray"))
-    (((class color)
-      (background light))
-     (:background  "#98FB98"))
-    (t
-     ()))
-  "*Face used by hl-line.")
-(setq hl-line-face 'hlline-face)
 (global-hl-line-mode)
 
 ;; カーソルの場所を保存する
