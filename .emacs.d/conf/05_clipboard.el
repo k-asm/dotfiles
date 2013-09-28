36a3ed9cba5095128403e5a498bd949d2992625c
Kosuke Asami <tfortress58@gmail.com>
2013-09-28 10:29:31 +0900

    emacs: if run-x, use xsel

--- .emacs.d/conf/05_clipboard.el
+++ .emacs.d/conf/05_clipboard.el
@@ -19,6 +19,7 @@
       (setq interprogram-paste-function 'copy-from-osx)))
 
 ;; clipboard setting
+(if run-x
 (setq interprogram-paste-function      
       (lambda ()
         (shell-command-to-string "xsel -b -o")))
@@ -27,5 +28,5 @@
         (let* ((process-connection-type nil)
                (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
           (process-send-string proc text)
-          (process-send-eof proc))))
+          (process-send-eof proc)))))
 
