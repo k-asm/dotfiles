ef613cc86445aaeacacef3446b6835c83045f10c
Kosuke Asami <tfortress58@gmail.com>
2013-09-28 11:41:39 +0900

    emacs: use ibus.el or mozc.el depending on window system

--- .emacs.d/conf/04_ibus.el
+++ .emacs.d/conf/04_ibus.el
@@ -1,8 +1,23 @@
 (if run-linux
-    (progn
+    (if run-x
+        ;; x
+        (progn
+          ;; need apt ibus-el
       (require 'ibus)
       (ibus-define-common-key ?\C-\s nil)
       (ibus-define-common-key ?\C-/ nil)
       (add-hook 'after-init-hook 'ibus-mode-on)
       (setq ibus-cursor-color '("limegreen" "white" "blue"))
-      (define-key global-map (kbd "C-\\") 'ibus-toggle)))
+      (define-key global-map (kbd "C-\\") 'ibus-toggle))
+      ;; terminal
+      (progn
+        ;; need apt emacs-mozc emacs-mozc-bin
+        (require 'mozc)
+        (set-language-environment "Japanese")
+        (setq default-input-method "japanese-mozc")
+        (setq mozc-candidate-style 'overlay)
+        (define-key global-map (kbd "C-\\") 'toggle-input-method)
+        ;; nn -> ん
+        (setq quail-japanese-use-double-n t)
+        ;; dired で文字化けする
+        (prefer-coding-system 'utf-8))))
