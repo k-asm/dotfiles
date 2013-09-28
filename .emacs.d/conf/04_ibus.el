bffca5683153138728f90e7c8e2b1dbca0cb4c1a
Kosuke Asami <tfortress58@gmail.com>
2013-09-28 11:42:18 +0900

    Indent

--- .emacs.d/conf/04_ibus.el
+++ .emacs.d/conf/04_ibus.el
@@ -3,12 +3,12 @@
         ;; x
         (progn
           ;; need apt ibus-el
-      (require 'ibus)
-      (ibus-define-common-key ?\C-\s nil)
-      (ibus-define-common-key ?\C-/ nil)
-      (add-hook 'after-init-hook 'ibus-mode-on)
-      (setq ibus-cursor-color '("limegreen" "white" "blue"))
-      (define-key global-map (kbd "C-\\") 'ibus-toggle))
+          (require 'ibus)
+          (ibus-define-common-key ?\C-\s nil)
+          (ibus-define-common-key ?\C-/ nil)
+          (add-hook 'after-init-hook 'ibus-mode-on)
+          (setq ibus-cursor-color '("limegreen" "white" "blue"))
+          (define-key global-map (kbd "C-\\") 'ibus-toggle))
       ;; terminal
       (progn
         ;; need apt emacs-mozc emacs-mozc-bin
