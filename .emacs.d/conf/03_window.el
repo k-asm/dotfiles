bfb51747fc8a6d23b4381bf6731317a600c44ba7
Kosuke Asami <tfortress58@gmail.com>
2013-09-28 10:46:28 +0900

    emacs: optimize window settings

--- .emacs.d/conf/03_window.el
+++ .emacs.d/conf/03_window.el
@@ -1,26 +1,26 @@
-(if run-x
+(if (display-graphic-p)
     (progn
-      (setq initial-frame-alist
-            (append (list '(width . 100)
-                          '(height . 40))
-                    initial-frame-alist))
-      (set-default-font "ricty-13.5")))
-
-(if (and run-cocoa (display-graphic-p))
-    (progn
-      (setq initial-frame-alist
-            (append (list '(width . 160)
-                          '(height . 45))
-                    initial-frame-alist))
-      (set-default-font "ricty-14")))
-
-(if (and (>= emacs-major-version 23) (display-graphic-p))
-    (progn
-      (set-fontset-font (frame-parameter nil 'font)
-                        'japanese-jisx0208
-                        '("ricty" . "unicode-bmp"))
-      (setq initial-frame-alist
-            (append (list '(alpha . 95)) initial-frame-alist))))
+      (cond (run-linux
+             (progn
+               (setq initial-frame-alist
+                     (append (list '(width . 100)
+                                   '(height . 40))
+                             initial-frame-alist))
+               (set-default-font "ricty-13.5")))
+            (run-darwin
+             (progn
+               (setq initial-frame-alist
+                     (append (list '(width . 160)
+                                   '(height . 45))
+                             initial-frame-alist))
+               (set-default-font "ricty-14"))))
+      (if (>= emacs-major-version 23)
+          (progn
+            (set-fontset-font (frame-parameter nil 'font)
+                              'japanese-jisx0208
+                              '("ricty" . "unicode-bmp"))
+            (setq initial-frame-alist
+                  (append (list '(alpha . 95)) initial-frame-alist))))))
 
 (setq default-frame-alist initial-frame-alist)
 (setq frame-title-format (format "emacs@%s : %%f" (system-name)))
