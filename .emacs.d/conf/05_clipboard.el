cd887cb80eaf85a6dbcd8b22b10d380f74abad40
Kosuke Asami <tfortress58@gmail.com>
2013-09-28 10:24:14 +0900

    emacs: cancel a commented out setting

--- .emacs.d/conf/05_clipboard.el
+++ .emacs.d/conf/05_clipboard.el
@@ -19,13 +19,13 @@
       (setq interprogram-paste-function 'copy-from-osx)))
 
 ;; clipboard setting
-;; (setq interprogram-paste-function      
-;;       (lambda ()
-;;         (shell-command-to-string "xsel -b -o")))
-;; (setq interprogram-cut-function      
-;;       (lambda (text &optional rest)
-;;         (let* ((process-connection-type nil)
-;;                (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
-;;           (process-send-string proc text)
-;;           (process-send-eof proc))))
+(setq interprogram-paste-function      
+      (lambda ()
+        (shell-command-to-string "xsel -b -o")))
+(setq interprogram-cut-function      
+      (lambda (text &optional rest)
+        (let* ((process-connection-type nil)
+               (proc (start-process "xsel" "*Messages*" "xsel" "-b" "-i")))
+          (process-send-string proc text)
+          (process-send-eof proc))))
 
