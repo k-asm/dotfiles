3d376d01f54732600b73cfadead01ffc159e7f03
Kosuke Asami <tfortress58@gmail.com>
2013-12-19 14:00:15 +0900

    emacs: Set UTF-8 as a prefer coding

--- .emacs.d/conf/00_init.el
+++ .emacs.d/conf/00_init.el
@@ -15,4 +15,5 @@
 (set-clipboard-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
