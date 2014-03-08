1ff506a5991b0aeed9ed8f59e1a91d133ce12207
Kosuke Asami <tfortress58@gmail.com>
2014-03-08 12:02:19 +0900

    emacs: Rename a function using 'my/' prefix

--- .emacs.d/conf/14_magit.el
+++ .emacs.d/conf/14_magit.el
@@ -5,14 +5,11 @@
   (window-configuration-to-register :magit-fullscreen)
   ad-do-it
   (delete-other-windows))
-
-(defun magit-quit-session ()
+(defun my/magit-quit-session ()
   "Restores the previous window configuration and kills the magit buffer"
   (interactive)
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
-
-(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
-
 (defadvice git-commit-comment (after move-to-magit-buffer activate)
+(define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
   (delete-window))
