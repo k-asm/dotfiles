8d4bb316d1e786fc32de9b03a30a1d5c775ac938
Kosuke Asami <asami@thinkpad>
2014-03-13 12:51:12 +0900

    emacs: ibus and mozc is not always necessary

--- .emacs.d/conf/04_ibus.el
+++ .emacs.d/conf/04_ibus.el
@@ -1,18 +1,28 @@
+;; from http://www.emacswiki.org/emacs/LoadingLispFiles
+(defmacro with-library (symbol &rest body)
+      `(condition-case nil
+           (progn
+             (require ',symbol)
+             ,@body)
+         (error (message (format "I guess we don't have %s available." ',symbol))
+                nil)))
+    (put 'with-library 'lisp-indent-function 1)
+
 (if run-linux
     (if run-x
         ;; x
         (progn
           ;; need apt ibus-el
-          (require 'ibus)
+          (with-library ibus
           (ibus-define-common-key ?\C-\s nil)
           (ibus-define-common-key ?\C-/ nil)
           (add-hook 'after-init-hook 'ibus-mode-on)
           (setq ibus-cursor-color '("limegreen" "white" "blue"))
-          (define-key global-map (kbd "C-\\") 'ibus-toggle))
+          (define-key global-map (kbd "C-\\") 'ibus-toggle)))
       ;; terminal
       (progn
         ;; need apt emacs-mozc emacs-mozc-bin
-        (require 'mozc)
+        (with-library mozc
         (set-language-environment "Japanese")
         (setq default-input-method "japanese-mozc")
         (setq mozc-candidate-style 'overlay)
@@ -20,4 +30,4 @@
         ;; nn -> ん
         (setq quail-japanese-use-double-n t)
         ;; dired で文字化けする
-        (prefer-coding-system 'utf-8))))
+        (prefer-coding-system 'utf-8)))))
