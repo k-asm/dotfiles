37f72edb09fbeb317d894ca0b4d036df7d143ca2
Kosuke Asami <tfortress58@gmail.com>
2014-03-24 13:01:47 +0900

    Fix to change directory using absolute path

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 cd $HOME
 git clone https://github.com/clear-code/zsh.d.git ~/.zsh.d
 
-cd $(dirname $0)
 cd $HOME
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+cd $(readlink -f $(dirname $0))
 for dotfile in .?*
 do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ]
